dff5b2a4025822d89e021a1e405cb924c78f4728
assuntaDC <decaroassunta98@gmail.com>
2021-07-09 20:01:49 +0200

    Release 1.0.0

--- Progetto6/src/adaptiveProducerFramework/test/ProducerCreatorTest.java
+++
@@ -1,7 +0,0 @@
-package adaptiveProducerFramework.test;
-
-import adaptiveProducerFramework.producers.Producer;
-
-public interface ProducerCreatorTest {
-	public Producer createProducer(String destination, String acceptorAddress, boolean pollingServiceTest);
-}
